90f214f4ad78bd59b9541164d6f157f57baff9a3
Niklas Held <xn.held@gmail.com>
2021-06-28 23:47:23 +0200

    Added logging functionality

--- bot.py
+++ bot.py
@@ -2,12 +2,27 @@ import requests
 from notify import TelegramBot
 from datetime import date, datetime
 from time import sleep
+import logging
 
 from config import config
 
 telegram_bot = TelegramBot(
     token=config["telegram"]["token"],
     chat_id=config["telegram"]["chat_id"])
+
+
+# Configure loggers
+logger = logging.getLogger('logger')
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler('application.log')
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.addHandler(fh)
 
 
 def check_appointments() -> None:
@@ -23,7 +38,7 @@ def check_appointments() -> None:
     result = requests.get(appointment_endpoint, cookies=get_cookies()).json()
 
     for available_date in result["available_days"]:
-        print(available_date)
+        logger.info(available_date)
         appointment_date = available_date["day"]
         telegram_bot.send_message(f"Earlier appointment available at \
             {appointment_date}!")
@@ -48,8 +63,8 @@ while True:
             (current_hour < night_end or current_hour >= night_start)) or
         (night_start < night_end and
             current_hour >= night_start and current_hour < night_end)):
-        print(f"Next check in {config['interval']['night']} minutes")
+        logger.info(f"Next check in {config['interval']['night']} minutes")
         sleep(config['interval']['night'] * 60)
     else:
-        print(f"Next check in {config['interval']['day']} minutes")
+        logger.info(f"Next check in {config['interval']['day']} minutes")
         sleep(config['interval']['day'] * 60)
